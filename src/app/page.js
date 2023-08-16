8e78cb7567201929cfa6d60294cedeabc8ad4015
LucasSimbelis <lucas.simbelis.teixeira@gmail.com>
2023-08-15 22:57:43 -0300

    versão inicial

---
+++ src/app/page.js
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <nav className="flex p-4 bg-amber-900">
+      <ul className="flex gap-20">
+        <li>
+          <a href="#">
+            <h1>Fiap Filmes</h1>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            filmes
+          </a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
